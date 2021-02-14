705c746ebaf60d5a00c01b0e3d3171a710c6c8f1
Tushar Patil <tusharpatilmarch@gmail.com>
2021-02-14 13:51:39 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,12 +5,13 @@
 # Any installation related commands
 #Install python 3.7
 sudo apt update
-sudo apt install software-properties-common
-sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt install -y software-properties-common
+sudo add-apt-repository -y ppa:deadsnakes/ppa
 sudo apt update
 sudo apt  install -y python3.7
+sudo apt install -y python3-pip
 #upgrade pip
 python3 -m pip install --upgrade pip
 
 #Install requirememts from backend
-pip install -r backend/app/requirements.txt
+pip3 install -r backend/app/requirements.txt
